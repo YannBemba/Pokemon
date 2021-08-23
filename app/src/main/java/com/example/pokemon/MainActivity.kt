b411e40d0e1b80228fdc84f2d680aae8fb6eaa4c
Yann Bemba Longa <yannbembacontact@gmail.com>
2021-08-23 13:09:02 +0200

    SearchBar & PokemonListScreen

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.example.pokemon.pokemonlist.PokemonListScreen
 import com.example.pokemon.ui.theme.PokemonTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,7 +33,9 @@ class MainActivity : ComponentActivity() {
                     startDestination = "pokemon_list_screen",
                 ) {
                     composable("pokemon_list_screen") {
-
+                        PokemonListScreen(
+                            navController = navController,
+                        )
                     }
                     composable(
                         "pokemon_detail_screen/{dominantColor}/{pokemonName}",
